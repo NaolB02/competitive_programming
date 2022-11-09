11179ddc2afbcc972b1ffb43ed5150deabaf0a40
NaolB02 <106165221+NaolB02@users.noreply.github.com>
2022-11-09 15:12:59 +0300

    Time: 161 ms (51.27%), Space: 14.4 MB (42.81%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,26 +1,16 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        curPeople = 0
-        minFrom = float('inf')
-        maxTo = 0
-        
-        for i in range(len(trips)):
-            if trips[i][1] < minFrom:
-                minFrom = trips[i][1]
-            if trips[i][2] > maxTo:
-                maxTo = trips[i][2]
-        
-        trips.sort(key = lambda x: x[1])
+        curPeople = 0      
+        loc = {}
         
         
-        for i in range(minFrom, maxTo + 1):
-            for j in range(len(trips)):
-                if i == trips[j][1]:
-                    curPeople += trips[j][0]
-                if i == trips[j][2]:
-                    curPeople -= trips[j][0]
-                
-                if curPeople > capacity:
-                    print(i, j)
-                    return False
+        
+        for people, start, end in trips:
+            loc[start] =loc.get(start,0)+ people
+            loc[end] =loc.get(end,0)- people
+        
+        for i in sorted(loc):
+            curPeople += loc[i]
+            if curPeople > capacity:
+                return False
         return True
