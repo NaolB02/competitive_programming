57e6a58cae0b6949dd1f5adda3c7d621cfbc07ac
NaolB02 <106165221+NaolB02@users.noreply.github.com>
2022-12-23 06:59:16 -0800

    Time: 62 ms (22.43%), Space: 13.8 MB (56.34%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -0,0 +1,19 @@
+class Solution:
+    def interpret(self, command: str) -> str:
+        interpreted = ''
+        iterator = 0
+        
+        while iterator < len(command):
+            if command[iterator] == "G":
+                interpreted += "G"
+                iterator += 1
+            
+            elif iterator + 1 < len(command) and command[iterator: iterator + 2] == "()":
+                interpreted += "o"
+                iterator += 2
+            
+            else:
+                interpreted += "al"
+                iterator += 4
+        
+        return interpreted
