755f0bd507c3342fbc71acb7c1eeab04189df871
NaolB02 <106165221+NaolB02@users.noreply.github.com>
2022-12-27 18:32:04 +0300

    A HackerRank Problem

---
+++ SetUnion.py
@@ -0,0 +1,8 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+nEnglish = int(input())
+english = set(map(int, input().split()))
+nFrench = int(input())
+french = set(map(int, input().split()))
+
+franco_english = english | french
+print(len(franco_english))
