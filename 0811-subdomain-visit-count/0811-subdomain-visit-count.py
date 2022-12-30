99ea1756f1355758655c6b765170fa885513452b
NaolB02 <106165221+NaolB02@users.noreply.github.com>
2022-12-30 18:35:43 +0300

    Time: 57 ms (87.27%), Space: 13.8 MB (98.42%) - LeetHub

--- 0811-subdomain-visit-count/0811-subdomain-visit-count.py
+++ 0811-subdomain-visit-count/0811-subdomain-visit-count.py
@@ -1,13 +1,18 @@
 class Solution:
     def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+        # stores the domain and times it was visited
         subDomainDict = defaultdict(int)
         arr = []
         
         for cpdomain in cpdomains:
             freq, dom = cpdomain.split()
+            # records the times the whole domain was visited
             subDomainDict[dom] += int(freq)
+            # splits the subdomains
             domList = list(dom.split('.'))
             
+            # checks if the domain has 3 subdomains or 2 
+            # and records each subdomain and the number of times they are visited
             if len(domList) == 2:
                 subDomainDict[domList[1]] += int(freq)
             
