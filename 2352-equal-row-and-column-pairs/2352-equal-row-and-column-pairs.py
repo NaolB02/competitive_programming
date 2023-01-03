4f4602a685a93f2264c8f4a399a4f04c331f46d1
NaolB02 <106165221+NaolB02@users.noreply.github.com>
2023-01-03 19:37:41 +0300

    Time: 737 ms (73.51%), Space: 19.1 MB (29.73%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -2,7 +2,9 @@ class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
         size = len(grid)
         countOfEqualPairs = 0
+        # This dictionary stores tuple form of the rows and their occurences
         rowDict = defaultdict(int)
+        # This dictionary stores tuple form of the columns and their occurences
         colDict = defaultdict(int)
         
         for row in grid:
@@ -16,8 +18,10 @@ class Solution:
             
             colDict[tuple(column)] += 1
         
+        # This block checks if a tuple is both a row and column
         for tup in rowDict:
             if tup in colDict:
+                # The number of possible pairs is calculated
                 countOfEqualPairs += (rowDict[tup] * colDict[tup])
         
         return countOfEqualPairs
