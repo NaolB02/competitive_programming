c04a2c3db1a5219cbb04765c4bfa2f75bb07a31f
Naol Belete <106165221+NaolB02@users.noreply.github.com>
2023-01-19 23:17:06 +0300

    Time: 36 ms (59.18%), Space: 13.8 MB (62.15%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,18 +1,15 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        pointer1, pointer2 = 0, 0
-        merged = ''
-        
-        # even if one of the strings is iterated fully 
-        # the other will continue to be iterated
-        while pointer1 < len(word1) or pointer2 < len(word2):
-            if pointer1 < len(word1):
-                merged += word1[pointer1]
-                pointer1 += 1
-            
-            if pointer2 < len(word2):
-                merged += word2[pointer2]
-                pointer2 += 1
-            
-        return merged
-        
+        word_temp = []
+
+        for i in range(len(word1)):
+            if i in range(len(word2)):
+                word_temp.append(word1[i])
+                word_temp.append(word2[i])
+            elif i:
+                word_temp.append(word1[i])
+
+        if len(word1) < len(word2):
+            word_temp.append(word2[len(word1):])         
+
+        return ''.join(word_temp)
