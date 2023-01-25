8fe655fb510572b55ae80e38998d657829a6dd19
Naol Belete <106165221+NaolB02@users.noreply.github.com>
2023-01-25 17:42:51 +0300

    Time: 172 ms (34.20%), Space: 15.7 MB (14.35%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -3,20 +3,28 @@ class Solution:
         left = 0
         k = 1
         
-        for right in range(1, len(nums)):
-            if nums[right] == nums[left]:
-                nums[right] = "_"
-            
-            else:
-                left = right
+        for right in range(len(nums)):
+            if nums[right] != nums[left]:
+                left += 1
+                nums[left] = nums[right]
                 k += 1
-                
-        holder = 0
-        
-        for seeker in range(len(nums)):
-            if nums[seeker] != "_":
-                nums[holder], nums[seeker] = nums[seeker], nums[holder]
-                holder += 1
         
         return k
+        
+#         for right in range(1, len(nums)):
+#             if nums[right] == nums[left]:
+#                 nums[right] = "_"
+            
+#             else:
+#                 left = right
+#                 k += 1
+                
+#         holder = 0
+        
+#         for seeker in range(len(nums)):
+#             if nums[seeker] != "_":
+#                 nums[holder], nums[seeker] = nums[seeker], nums[holder]
+#                 holder += 1
+        
+#         return k
     
