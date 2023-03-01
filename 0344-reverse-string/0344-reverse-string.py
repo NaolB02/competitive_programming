483bd99ded15ba8f9ac729c77dee41c348454a5e
Naol Belete <106165221+NaolB02@users.noreply.github.com>
2023-03-01 07:37:03 +0300

    Time: 215 ms (29.71%), Space: 43.1 MB (5.48%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,11 +3,11 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        start, end = 0, len(s) - 1
+        def revString(s, p1 = 0, p2 = len(s) - 1):
+            if p1 >= p2:
+                pass
+            else:
+                s[p1], s[p2] = s[p2], s[p1]
+                revString(s, p1 + 1, p2 - 1)
         
-        while start < end:
-            s[start], s[end] = s[end], s[start]
-            start += 1
-            end -= 1
-        
-        
+        revString(s, 0, len(s) - 1)
