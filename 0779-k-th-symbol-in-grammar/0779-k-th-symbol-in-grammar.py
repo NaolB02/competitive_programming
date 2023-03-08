8e0d008da2108b8db5a1e4b80c6dcc02ae7bf46b
Naol Belete <106165221+NaolB02@users.noreply.github.com>
2023-03-08 10:34:25 +0300

    Time: 33 ms (53.63%), Space: 13.7 MB (100.00%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
@@ -3,18 +3,10 @@ class Solution:
         if n == 1:
             return 0
 
-        else:
-            new_k = (k + 1) // 2
-            prev = self.kthGrammar(n - 1, new_k)
-            
-            if not prev:
-                if k % 2:
-                    return 0
-                else:
-                    return 1
-            
-            else:
-                if k % 2:
-                    return 1
-                else:
-                    return 0
+        new_k = (k + 1) // 2
+        prev = self.kthGrammar(n - 1, new_k)
+
+        if k % 2:
+            return prev
+
+        return 1 - prev
