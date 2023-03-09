1f7cfcdb340ba3affd4fd2f6b26355fa695be4f0
Naol Belete <106165221+NaolB02@users.noreply.github.com>
2023-03-09 14:17:30 +0300

    Time: 1118 ms (5.04%), Space: 15.9 MB (43.92%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -2,16 +2,21 @@ class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         combinations = []
         
-        def get_comb(initial, list1, n, k):
-            list1.append(initial)
-            
-            if len(list1) == k:
-                combinations.append(list1)
+        def comb(initial, lis, n, k):
+            if len(lis) == k:
+                combinations.append(lis)
                 return
             
-            for index in range(initial + 1, n + 1):
-                get_comb(index,[num for num in list1], n, k)
-        for index in range(1,n+1):
-            get_comb(index,[],n,k)
+            if initial > n:
+                return
+            
+            #case 1
+            comb(initial + 1, [num for num in lis], n, k)
+            
+            #case 2
+            lis.append(initial)
+            comb(initial + 1, [num for num in lis], n, k)
         
+        comb(1, [], n, k)
+    
         return combinations
