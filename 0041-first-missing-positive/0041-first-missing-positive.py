122cec7fd3a5fe944b65b71285bd504d6648132a
Naol Belete <106165221+NaolB02@users.noreply.github.com>
2023-03-26 22:50:25 +0300

    Time: 394 ms (19.48%), Space: 27.7 MB (42.97%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,5 +1,7 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
+        '''
+        -> cyclic approach
         # sorting nums cyclically
         for i in range(len(nums)):
             cur = nums[i]
@@ -14,3 +16,14 @@ class Solution:
                 return i + 1
         
         return i + 2
+        '''
+        
+        freq = [0] * (len(nums) + 1)
+        
+        for num in nums:
+            if num > 0 and num < len(nums) + 1:
+                freq[num - 1] += 1
+        
+        for i in range(len(freq)):
+            if freq[i] == 0:
+                return i + 1
