0d219a61040f2d12cf82b667c0506575ebe520f9
Naol Belete <106165221+NaolB02@users.noreply.github.com>
2023-03-30 18:54:46 +0300

    Time: 39 ms (55.89%), Space: 13.9 MB (70.36%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -1,13 +1,29 @@
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
-        ord_set = sorted(set(s))
+#         ord_set = sorted(set(s))
         
-        for i in ord_set:
-            index = s.index(i)
-            value = s[index:]
+#         for i in ord_set:
+#             index = s.index(i)
+#             value = s[index:]
             
-            if len(set(value)) == len(ord_set):
-                return i + self.removeDuplicateLetters(value.replace(i, ""))
+#             if len(set(value)) == len(ord_set):
+#                 return i + self.removeDuplicateLetters(value.replace(i, ""))
         
-        return ''
+#         return ''
+        stack = []
+        visited = set()
+        
+        last_occur = {}
+        for i, letter in enumerate(s):
+            last_occur[letter] = i
+        
+        for i, letter in enumerate(s):
+            if letter not in visited:
+                while stack and letter < stack[-1] and i < last_occur[stack[-1]]:
+                    visited.remove(stack.pop())
+                    
+                stack.append(letter)
+                visited.add(letter)
+        
+        return "".join(stack)
             
