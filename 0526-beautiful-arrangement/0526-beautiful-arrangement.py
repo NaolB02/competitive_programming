45e23ab5f6fc36f0a2d41d2c90525deb2c33b528
Naol Belete <106165221+NaolB02@users.noreply.github.com>
2023-04-09 22:51:47 +0300

    Time: 2225 ms (13.89%), Space: 13.9 MB (35.10%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.py
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.py
@@ -4,34 +4,14 @@ class Solution:
         count = 0
         arr = []
         
-        def check_beauty(arr):
-            for i in range(1, len(arr) + 1):
-                if i % arr[i - 1] == 0 or arr[i - 1] % i == 0:
-                    continue
-                
-                else:
-                    return False
-            
-            return True
-    
-        
         def backtrack(i):
             nonlocal n
-            nonlocal count
-            
-            # if i % arr[i - 1] == 0 or arr[i - 1] % i == 0:
-            
+            nonlocal count            
             
             if i == n + 1:
                 count += 1
                 return
             
-#             if i == n and check_beauty(arr):
-#                 count += 1
-#                 return
-            
-#             elif i == n:
-#                 return
             
             for j in range(1, n + 1):
                 if j not in visited:
