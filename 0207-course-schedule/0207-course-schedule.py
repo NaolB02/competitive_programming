3048e5679a0b1150cbed7e96f9762d65f9eb40e9
Naol Belete <106165221+NaolB02@users.noreply.github.com>
2023-05-10 17:35:12 +0300

    Time: 119 ms (16.72%), Space: 17.8 MB (21.31%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,34 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        dependencies = defaultdict(int)
+        graph = defaultdict(list)
+        
+        for i in range(numCourses):
+            graph[i]
+            dependencies[i]
+            
+        for cur, pre in prerequisites:
+            graph[pre].append(cur)
+            dependencies[cur] += 1
+        
+        frontier = deque()
+        for node in graph:
+            if dependencies[node] == 0:
+                frontier.append(node)
+        
+        topo_order = []
+        while frontier:
+            node = frontier.popleft()
+            
+            for neigh in graph[node]:
+                dependencies[neigh] -= 1
+                
+                if dependencies[neigh] == 0:
+                    frontier.append(neigh)
+            
+            topo_order.append(node)
+            del graph[node]
+        
+        if len(topo_order) != numCourses:
+            return False
+        return True
